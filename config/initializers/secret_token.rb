084e42804c7063586b11cd987c3c5a582eaa07d1
Brice Texier <brice.texier@ekylibre.org>
2012-07-03 17:36:25 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sagitta::Application.config.secret_token = '1a9d8ee8101c123cecca27fde2331a3290f1cccedcedb0285cfa077d3a9b009cebf5c79ff568b3cfd862762a4b6d1bc2cd1031d320d5d4bc4efc2647f61e8261'
