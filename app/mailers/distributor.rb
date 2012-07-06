ac8a8444e9c083ca2b654d3dce3627680c0010a1
Brice Texier <burisu@oneiros.fr>
2012-07-06 15:40:27 +0200

    Adds host

--- app/mailers/distributor.rb
+++ app/mailers/distributor.rb
@@ -3,8 +3,8 @@ class Distributor < ActionMailer::Base
   def news(recipient, communication)
     @communication = communication
     @recipient = recipient
-    @message_url = message_url(@communication.to_param)
-    @unsubscribe_url = unsubscribe_url(@communication.client_id, recipient)
+    @message_url = message_url(@communication.to_param, :host => "agrimail.fr")
+    @unsubscribe_url = unsubscribe_url(@communication.client_id, recipient, :host => "agrimail.fr")
     attachments.inline[@communication.flyer.original_filename] = File.read(@communication.flyer.path(:web))
     mail(:to => recipient, :from => @communication.from, :subject => @communication.subject)
   end
